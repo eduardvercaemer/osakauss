9a0e068319e5165cf4ab46a7fb921446d2031e79
InsaneMiner <gurued58@gmail.com>
2021-05-02 14:19:06 +0200

    Deleted junk file

--- simple.py
+++
@@ -1,9 +0,0 @@
-import re
-with open('Character.h','w') as h:
-	h.write('')
-with open('Character.h',"ab") as f:
-	f.write(b"const char[128] = {")
-	for x in [chr(i) for i in range(128)]:
-		BYTE = ord(x)
-		f.write( bytes(r"'" + fr"{BYTE}" + r"'"+ ",\n","utf-8") )
-	f.write(b"}")
